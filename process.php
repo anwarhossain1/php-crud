cb7d34ccd70243b8379c4b6c1ed6d38c31f334d2
anwarhossain1 <irishkhan33@gmail.com>
2021-08-24 13:18:19 +0600

    delete functionalities has been added

--- process.php
+++ process.php
@@ -9,3 +9,8 @@ if(isset($_POST['save'])){
     $mysqli->query("INSERT INTO data (1stname, 2ndname) VALUES ('$name1', '$name2')") or die($mysqli->error);
 
 }
+
+if(isset($_GET['delete'])){
+    $id = $_GET['delete'];
+    $mysqli->query("DELETE FROM data WHERE id=$id") or die(mysqli->error);
+}
